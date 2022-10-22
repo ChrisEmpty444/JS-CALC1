9e345cdab61eb97bcf418038541fe25c68b44505
Alunos - Senai 6.02 <alunos@educ602.local>
2022-10-22 13:43:28 -0300

    INICIANDO PROJETO UTILIZANDO JS

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const previusOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll ("#buttons-container button");
+
+class Calculator {}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        console.log(value);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
